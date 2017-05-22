1f421223e42735dce485cb222f2c6493940c5486
Betsy <825614102@qq.com>
2017-05-22 18:03:09 +0430

    modify model to find cycles

--- readGraph.py
+++ readGraph.py
@@ -18,42 +18,43 @@ def read_save_DAG(filename):
     f.close()
     return Graph
 
+def findCircleGraph(G, component, group_code, num_c, component_code):
+    circle_graph = []
+    g = G.subgraph(component)
+    for scc in nx.strongly_connected_components(g):
+        if len(scc) > 1:
+            g2 = g.subgraph(scc)
+            for start_node, end_node in g2.edges():
+                circle_graph.append((start_node, end_node))
+            group_code += 1
+    return circle_graph, group_code
+
+def find_cycle(G):
+    circles = []
+    group_code, component_code = 0, 0
+    components = nx.weakly_connected_components(G)
+    for component in components:
+	num_c = len(component)
+        # find circle graph
+        circle_graph, group_code = findCircleGraph(G, component, group_code, num_c, component_code)
+	#print circle_graph
+        circles += circle_graph
+    return circles	
+
 if __name__ == "__main__":
     start_time = time.time()
-    Graph = read_save_DAG(sys.argv[1])
-    print "loading data is finished in", (time.time() - start_time) / 1000, "s"
-    print "the number of nodes is", Graph.number_of_nodes()
-    print "the number of edge is", Graph.number_of_edges()
-    
+    G = read_save_DAG(sys.argv[1])
+    ## print the basis infomation
+    print "loading data is finished in", (time.time() - start_time), "s"
+    print "the number of nodes is", G.number_of_nodes()
+    print "the number of edge is", G.number_of_edges()
     start_time = time.time()
-    #cycles = list(nx.algorithms.find_cycle(Graph))
-    fp = open("save_cycles.txt", 'w')
-    '''
-    fp = open("save_cycles.txt", 'w')
-    for i in range(len(cycles)):
-	fp.write(str(cycles[i])+"\n")
-    '''
 
-    s_components = nx.strongly_connected_components(Graph)
-    #cycles = []
-    #print "first", list(s_components)
-    #print "over", list(s_components)
-    for s_component in list(s_components):
-        #print "len is", len(s_component)
-	cycles = []
-	if len(s_component) == 1:
-	    #print s_component, 'hhh'
-	    continue
-	else:
-	    #print s_component , "ppp"
-	    sub_graph = Graph.subgraph(list(s_component))
-	    for cycle in nx.algorithms.simple_cycles(sub_graph):
-		cycles.append(cycle)
-	    for i in range(len(cycles)):
-		fp.write(str(cycles[i])+"\n")
-    #print "over2"
-    #fp = open("save_cycles.txt", 'w')
-    #for i in range(len(cycles)):
-    #    fp.write(str(cycles[i])+"\n")
+    circles = find_cycle(G)
+    #save the cycles
+    fp = open("save_cycles.txt", 'w')
+    for c in circles:
+        fp.write(str(c)+"\n")
     fp.close()
-    print "finding circles is finished in", (time.time() - start_time) / 1000, "s"
+    print "finding circles is finished in", (time.time() - start_time), "s"
+
